d764c3c9f03767da40526b930cf295b82dc768c7
Alief Muhammad Abdillah <aliefmabdillah09@gmail.com>
2026-09-15 12:39:34 +0700

    feat: dynamic checklist

--- src/components/generate/SectionChecklistBase.tsx
+++ src/components/generate/SectionChecklistBase.tsx
@@ -1,66 +1,95 @@
 "use client";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ComponentCard from "../common/ComponentCard";
 import { useFormContext } from "react-hook-form";
 import Input from "../form/input/react-hook/InputFieldHook";
 import Checkbox from "../form/input/react-hook/CheckboxHook";
+import { getIdDevice } from "@/lib/sessions";
 
 const checklist = [
   {
     text: "Pembersihan fisik dan pengecekan kebocoran shelter",
     name: "pembersihan_fisik",
+    type: "fixed",
   },
   {
     text: "Pengecekan dan Pembersihan",
     name: "pengecekan_pembersihan",
+    type: "all",
   },
   {
     text: "Pengecekan sensor gas dan chamber gas",
     name: "sensor_gas",
+    type: "fixed",
   },
   {
     text: "Pengecekan sensor meteorologi",
     name: "sensor_meteorologi",
+    type: "all",
   },
   {
     text: "Pengecekan sensor partikulat",
     name: "sensor_partikulat",
+    type: "all",
   },
   {
     text: "Pengecekan kondisi kabel dan koneksi (power dan data)",
     name: "kabel_koneksi",
+    type: "all",
   },
   {
     text: "Pencatatan / Backup data sensor (raw data)",
     name: "pencatatan_backup_data",
+    type: "all",
   },
   {
     text: "Pengecekan catu daya dan sistem back-up (UPS/baterai)",
     name: "catu_daya",
+    type: "fixed",
   },
   {
     text: "Pengecekan fungsi komunikasi data (modem/ethernet)",
     name: "komunikasi_data",
+    type: "all",
   },
   {
     text: "Penggantian silika gel",
     name: "silika_gel",
+    type: "fixed",
   },
   {
     text: "Pengecekan display running text",
     name: "display_running_text",
+    type: "fixed",
   },
   {
     text: "Pengecekan pagar pelindung",
     name: "pagar_pelindung",
+    type: "fixed",
   },
   {
     text: "Pengecekan CCTV",
     name: "cctv",
+    type: "fixed",
   },
 ];
 export default function SectionChecklistBase() {
   const { register } = useFormContext();
+  const [idDevice, setIdDevice] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchIdDevice = async () => {
+      const id = await getIdDevice();
+      setIdDevice(id?.toLowerCase() ?? "");
+    };
+
+    fetchIdDevice();
+  }, []);
+
+  const visibleChecklist = checklist.filter(
+    (item) => idDevice?.includes("base") || (idDevice?.includes("mini") && item.type === "all"),
+  );
+
   return (
     <ComponentCard title={`Checklist Maintenance`}>
       <div className="grid grid-cols-3 mb-4 text-gray-800 dark:text-white/90">
@@ -68,7 +97,7 @@ export default function SectionChecklistBase() {
         <p className="px-2 text-center">Status</p>
         <p className="px-2 text-center">Keterangan</p>
       </div>
-      {checklist.map((item, index) => (
+      {visibleChecklist.map((item, index) => (
         <div
           key={index}
           className="grid grid-cols-3 items-center py-2 gap-5 text-gray-700 dark:text-gray-400"
